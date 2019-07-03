f7fde3d22f450b8bce68500e51c5766454418886
Mehdi Sheykhveysi <m.sheykhveysi4680@gmail.com>
2019-07-03 19:50:01 +0430

    implement Proxsy Pattern

---
+++ ProxyPatterns/ImageProxy.cs
@@ -0,0 +1,22 @@
+﻿namespace ProxyPattern
+{
+    public class ImageProxy : IImage
+    {
+        public ImageProxy(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+
+        public string FilePath { get; set; }
+
+        public RealImage RealImage { get; set; }
+
+        public void Display()
+        {
+            if (RealImage == null)
+                RealImage = new RealImage(FilePath);
+
+            RealImage.Display();
+        }
+    }
+}
---
+++ ProxyPatterns/RealImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProxyPattern
+{
+    public class RealImage:IImage
+    {
+        public RealImage(string filePath)
+        {
+            this.FilePath = filePath;
+        }
+        public string FilePath { get; set; }
+
+        public void Display() => Console.WriteLine($"File Path is {this.FilePath}");
+    }
+}
